4732881b945fd15957478b951b928fac06804e34
Miles Henrichs <mileshdesign@gmail.com>
2018-04-11 16:50:42 -0500

    Header component, logo & favicon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Favicon from 'react-favicon';
+import appFavicon from './resources/favicon.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Favicon url={appFavicon} />
+        <Header />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../resources/logo.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+      	<div className="container">
+      		<a href="#">
+      			<img src={logo} alt="EasyCal" />
+      		</a>
+      		<nav>
+      			<a href="#" className="nav__today-link">Today (2150 cal)</a>
+      			<a href="#">Stats</a>
+      			<a href="#">My Account</a>
+      		</nav>
+      		<div className="clearfix"></div>
+      	</div>
+      </header>
+    );
+  }
+}
+
+export default Header;
